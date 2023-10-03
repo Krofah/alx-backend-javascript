8e3bf6724f7640c3f961ac942dbdd329f56d798c
Krofah <amponsahkrofah@gmail.com>
2023-10-03 22:31:42 +0000

    task 0

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,19 +1,13 @@
-function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
-    throw new Error('Cannot process');
-  }
-
-  const updatedMap = new Map();
-
-  for (const [item, quantity] of map) {
-    if (quantity === 1) {
-      updatedMap.set(item, 100);
-    } else {
-      updatedMap.set(item, quantity);
+const updateUniqueItems = (map) => {
+  if (map instanceof Map) {
+    for (const [key, value] of map.entries()) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
     }
+    return map;
   }
-
-  return updatedMap;
-}
+  throw new Error('Cannot process');
+};
 
 export default updateUniqueItems;
